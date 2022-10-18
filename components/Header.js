e4ba75814d303b9ecdc6506c3d6a551c05095b9c
Joshua Fendi <joshuafendi@gmail.com>
2022-10-17 21:32:20 -0700

    headshot

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,14 @@
 import headerStyles from "../styles/Header.module.css";
 import NavBar from "./NavBar";
 import Image from "next/image";
+import headshot from "../public/Headshot.jpg"
 
 const Header = ({ children }) => {
   return (
     <div>
       <div>
         <h1 className={headerStyles.title}>
-          <Image src="/../public/Headshot.jpg" width={500} height={250} />
+          <Image src={headshot} alt="headshot" width={500} height={250} />
           <div className={headerStyles.hello}>Hello, my name is </div>
           <span>Joshua Fendi</span>
         </h1>
